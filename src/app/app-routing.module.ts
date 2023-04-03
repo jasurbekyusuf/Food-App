2ebc578a9862897fc691552277e28d9aecb79824
Jasurbek Yusufov <jasurbekyusufov@gmail.com>
2023-04-03 10:13:07 +0500

    COMPONENTS: Add Contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'menu',component:MenuComponent},
   {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent}
 ];
 
 @NgModule({
