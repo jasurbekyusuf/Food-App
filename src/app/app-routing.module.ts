12b02720c059592d6b25e49ff759796462da2b6d
Jasurbek Yusufov <jasurbekyusufov@gmail.com>
2023-04-03 10:41:10 +0500

    COMPONENTS: Add Menu Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { MenupageComponent } from './pages/menupage/menupage.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'menu',component:MenuComponent},
   {path:'about',component:AboutComponent},
+  {path:'menu/:id',component:MenupageComponent},
   {path:'contact',component:ContactComponent}
 ];
 
